0d56dad229e8994fc00505ef766405727bf45e78
Stuart Haidon <serayen.sh@gmail.com>
2021-12-29 19:25:14 +0000

    Data Flow: Implement Dup and DupPair.

--- src/program/data_flow/stack_ops.rs
+++ src/program/data_flow/stack_ops.rs
@@ -6,6 +6,85 @@ use crate::{
 
 use super::{generate_stack_exhaustion_diag, Analyzer, ConstVal, PtrId, ValueId};
 
+pub(super) fn dup(
+    stack: &mut Vec<ValueId>,
+    analyzer: &mut Analyzer,
+    op_idx: usize,
+    source_store: &SourceStorage,
+    op: &Op,
+    had_error: &mut bool,
+    depth: usize,
+) {
+    if stack.len() < (depth + 1) {
+        generate_stack_exhaustion_diag(source_store, op, stack.len(), depth + 1);
+        *had_error = true;
+        let (new_id, _) = analyzer.new_value(PorthTypeKind::Unknown, op_idx, op.token);
+        stack.push(new_id);
+        return;
+    }
+
+    let val_id = stack[stack.len() - 1 - depth];
+    let value = analyzer.value_mut(val_id);
+    let val_type = value.porth_type;
+    let val_const_val = value.const_val;
+
+    let (new_id, new_val) = analyzer.new_value(val_type, op_idx, op.token);
+    new_val.const_val = val_const_val;
+    stack.push(new_id);
+}
+
+pub(super) fn dup_pair(
+    stack: &mut Vec<ValueId>,
+    analyzer: &mut Analyzer,
+    op_idx: usize,
+    source_store: &SourceStorage,
+    op: &Op,
+    had_error: &mut bool,
+) {
+    match &**stack {
+        [.., a, b] => {
+            let [a_val, b_val] = analyzer.get_values([*a, *b]);
+            let a_type = a_val.porth_type;
+            let a_const = a_val.const_val;
+            let b_type = b_val.porth_type;
+            let b_const = b_val.const_val;
+
+            let (dup_a_id, dup_a) = analyzer.new_value(a_type, op_idx, op.token);
+            dup_a.const_val = a_const;
+            stack.push(dup_a_id);
+
+            let (dup_b_id, dup_b) = analyzer.new_value(b_type, op_idx, op.token);
+            dup_b.const_val = b_const;
+            stack.push(dup_b_id);
+        }
+        [a] => {
+            let a = *a;
+            generate_stack_exhaustion_diag(source_store, op, stack.len(), op.code.pop_count());
+            *had_error = true;
+
+            let (dup_b_id, _) = analyzer.new_value(PorthTypeKind::Unknown, op_idx, op.token);
+            stack.push(dup_b_id);
+
+            let [a_val] = analyzer.get_values([a]);
+            let a_type = a_val.porth_type;
+            let a_const = a_val.const_val;
+
+            let (dup_a_id, dup_a) = analyzer.new_value(a_type, op_idx, op.token);
+            dup_a.const_val = a_const;
+            stack.push(dup_a_id);
+        }
+        [] => {
+            generate_stack_exhaustion_diag(source_store, op, stack.len(), op.code.pop_count());
+            *had_error = true;
+
+            let (dup_b_id, _) = analyzer.new_value(PorthTypeKind::Unknown, op_idx, op.token);
+            stack.push(dup_b_id);
+            let (dup_a_id, _) = analyzer.new_value(PorthTypeKind::Unknown, op_idx, op.token);
+            stack.push(dup_a_id);
+        }
+    }
+}
+
 pub(super) fn swap(
     stack: &mut Vec<ValueId>,
     analyzer: &mut Analyzer,
