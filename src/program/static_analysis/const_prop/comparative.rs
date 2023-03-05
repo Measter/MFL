b06776a563307eb38f5ed4b3b3c8968c08512bf2
Stuart Haidon <serayen.sh@gmail.com>
2023-03-05 21:07:33 +0000

    Const Prop: Fix Int-Int equality assuming that the output is an integer.

--- src/program/static_analysis/const_prop/comparative.rs
+++ src/program/static_analysis/const_prop/comparative.rs
@@ -108,12 +108,21 @@ pub(super) fn equal(
 ) {
     let op_data = analyzer.get_op_io(op.id);
     let input_ids = *op_data.inputs.as_arr::<2>();
-    let Some([output_type_id]) = analyzer.value_types(*op_data.outputs.as_arr()) else { return };
+    let input_type_ids = analyzer.value_types(input_ids).unwrap();
     let Some(input_const_vals) = analyzer.value_consts(input_ids) else { return };
 
     let new_const_val = match input_const_vals {
         [ConstVal::Int(a), ConstVal::Int(b)] => {
-            let TypeKind::Integer { width: output_width, signed: output_sign }  = type_store.get_type_info(output_type_id).kind else {unreachable!()};
+            let [
+                TypeKind::Integer { width: a_width, signed: a_signed },
+                TypeKind::Integer { width: b_width, signed: b_signed }
+            ] = input_type_ids.map(|id| type_store.get_type_info(id).kind) else {
+                unreachable!()
+            };
+
+            // If we got here then the cast already type-checked.
+            let (output_sign, output_width) =
+                promote_int_type(a_width, a_signed, b_width, b_signed).unwrap();
 
             // If we got here then the cast already type-checked.
             let a_kind = a.cast(output_width, output_sign);
