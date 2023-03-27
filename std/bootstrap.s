44a2c8778fce43d848d7d6a6e8f099f7ace85f9e
Stuart Haidon <serayen.sh@gmail.com>
2023-03-27 20:49:19 +0100

    Implement _syscall7

--- std/bootstrap.s
+++ std/bootstrap.s
@@ -17,3 +17,16 @@ _syscall4:
     syscall
 
     ret
+
+global _syscall7
+_syscall7:
+    mov RAX, RDI
+    mov RDI, RSI
+    mov RSI, RDX
+    mov RDX, RCX
+    mov R10, R8
+    mov R8, R9
+    mov R9, [RSP+8]
+    syscall
+
+    ret
