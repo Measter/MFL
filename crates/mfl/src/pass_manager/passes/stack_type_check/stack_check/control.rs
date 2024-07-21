23e32835cd486cdad59b044f5ff549bb2bdd8fe5
Stuart Haidon <serayen.sh@gmail.com>
2024-07-21 23:27:35 +0100

    MFL: Invert logic in StackTypeCheck epilogue_return's stack length check This way it ensures that the op still has IO values set, even if the op was incorrect.

--- crates/mfl/src/pass_manager/passes/stack_type_check/stack_check/control.rs
+++ crates/mfl/src/pass_manager/passes/stack_type_check/stack_check/control.rs
@@ -37,62 +37,60 @@ pub(crate) fn epilogue_return(
     let item_sig = ctx.urir().get_item_signature(item_id);
 
     let exit_sig = &item_sig.exit.inner;
-    if stack.len() == exit_sig.len() {
-        let inputs = stack.lastn(exit_sig.len()).unwrap();
+    if stack.len() != exit_sig.len() {
+        let mut labels = vec![
+            Label::new(op.token.location)
+                .with_color(Color::Red)
+                .with_message("returning here"),
+            Label::new(item_sig.exit.location)
+                .with_color(Color::Cyan)
+                .with_message("return type defined here"),
+        ];
 
-        for &value_id in inputs {
-            analyzer.consume_value(value_id, op.id);
-        }
-        analyzer.set_op_io(op, inputs, &[]);
-        let len = inputs.len();
-        stack.truncate(stack.len() - len);
-
-        return;
-    }
-
-    had_error.set();
-
-    let mut labels = vec![
-        Label::new(op.token.location)
-            .with_color(Color::Red)
-            .with_message("returning here"),
-        Label::new(item_sig.exit.location)
-            .with_color(Color::Cyan)
-            .with_message("return type defined here"),
-    ];
-
-    match stack.len().cmp(&exit_sig.len()) {
-        Ordering::Less => {
-            let num_missing = usize::saturating_sub(exit_sig.len(), stack.len());
-            for _ in 0..num_missing {
-                let pad_value = analyzer.new_value(op.token.location, None);
-                stack.push(pad_value);
+        match stack.len().cmp(&exit_sig.len()) {
+            Ordering::Less => {
+                let num_missing = usize::saturating_sub(exit_sig.len(), stack.len());
+                for _ in 0..num_missing {
+                    let pad_value = analyzer.new_value(op.token.location, None);
+                    stack.push(pad_value);
+                }
             }
+            Ordering::Greater => {
+                let unused_values = stack[..stack.len() - exit_sig.len()]
+                    .iter()
+                    .zip(0..)
+                    .map(|(&id, idx)| (id, idx, "unused value"));
+                let unused_value_labels =
+                    build_creator_label_chain(analyzer, unused_values, Color::Green, Color::Cyan);
+                labels.extend(unused_value_labels);
+            }
+            Ordering::Equal => unreachable!(),
         }
-        Ordering::Greater => {
-            let unused_values = stack[..stack.len() - exit_sig.len()]
-                .iter()
-                .zip(0..)
-                .map(|(&id, idx)| (id, idx, "unused value"));
-            let unused_value_labels =
-                build_creator_label_chain(analyzer, unused_values, Color::Green, Color::Cyan);
-            labels.extend(unused_value_labels);
-        }
-        Ordering::Equal => unreachable!(),
+
+        diagnostics::emit_error(
+            stores,
+            op.token.location,
+            format!(
+                "function '{}' return {}, found {}",
+                stores.strings.resolve(item_header.name.inner),
+                exit_sig.len(),
+                stack.len()
+            ),
+            labels,
+            None,
+        );
+
+        had_error.set();
     }
 
-    diagnostics::emit_error(
-        stores,
-        op.token.location,
-        format!(
-            "function '{}' return {}, found {}",
-            stores.strings.resolve(item_header.name.inner),
-            exit_sig.len(),
-            stack.len()
-        ),
-        labels,
-        None,
-    );
+    let inputs = stack.lastn(exit_sig.len()).unwrap();
+
+    for &value_id in inputs {
+        analyzer.consume_value(value_id, op.id);
+    }
+    analyzer.set_op_io(op, inputs, &[]);
+    let len = inputs.len();
+    stack.truncate(stack.len() - len);
 }
 
 pub(crate) fn prologue(
