d4f0ea7b3a9188e7ce1937de4b901dbebc3ad428
Stuart Haidon <serayen.sh@gmail.com>
2024-07-23 16:36:59 +0100

    MFL: Check for recursive types.

--- crates/mfl/src/pass_manager/passes/cycles.rs
+++ crates/mfl/src/pass_manager/passes/cycles.rs
@@ -5,8 +5,9 @@ use crate::{
     context::{Context, ItemHeader, ItemId, ItemKind},
     diagnostics,
     error_signal::ErrorSignal,
-    ir::{NameResolvedOp, Op, OpCode},
+    ir::{NameResolvedOp, NameResolvedType, Op, OpCode},
     pass_manager::PassContext,
+    source_file::SourceLocation,
     Stores,
 };
 
@@ -36,9 +37,101 @@ fn check_invalid_cycles_structs(
     stores: &mut Stores,
     pass_ctx: &mut PassContext,
     had_error: &mut ErrorSignal,
-    cur_id: ItemId,
+    root_id: ItemId,
 ) {
-    // todo!()
+    let root_struct_def = ctx.nrir().get_struct(root_id);
+    let name_location = root_struct_def.name.location;
+    let mut check_queue = vec![root_id];
+    let mut checked_items = HashSet::new();
+
+    while let Some(item) = check_queue.pop() {
+        if item != root_id
+            && pass_ctx
+                .ensure_ident_resolved_signature(ctx, stores, item)
+                .is_err()
+        {
+            had_error.set();
+        }
+
+        let struct_def = ctx.nrir().get_struct(item);
+        for field in &struct_def.fields {
+            check_invalid_cyclic_refs_in_field_kind(
+                stores,
+                had_error,
+                root_id,
+                name_location,
+                field.name.location,
+                &field.kind,
+                &mut check_queue,
+                &mut checked_items,
+            );
+        }
+    }
+}
+
+fn check_invalid_cyclic_refs_in_field_kind(
+    stores: &Stores,
+    had_error: &mut ErrorSignal,
+    root_id: ItemId,
+    root_name_location: SourceLocation,
+    field_name_location: SourceLocation,
+    field_kind: &NameResolvedType,
+    check_queue: &mut Vec<ItemId>,
+    checked_items: &mut HashSet<ItemId>,
+) {
+    match field_kind {
+        NameResolvedType::SimpleCustom { id, .. }
+        | NameResolvedType::GenericInstance { id, .. } => {
+            #[allow(clippy::bool_comparison)]
+            if checked_items.insert(*id) == false {
+                return;
+            }
+            // False means that the value was already in the set.
+
+            if *id == root_id {
+                had_error.set();
+                diagnostics::emit_error(
+                    stores,
+                    field_name_location,
+                    "recursive struct",
+                    [
+                        Label::new(root_name_location)
+                            .with_color(Color::Red)
+                            .with_message("in this struct"),
+                        Label::new(field_name_location)
+                            .with_color(Color::Cyan)
+                            .with_message("in this field"),
+                    ],
+                    None,
+                );
+            } else {
+                check_queue.push(*id);
+            }
+        }
+        NameResolvedType::Array(id, _) => {
+            let mut inner_array_id = id;
+
+            while let NameResolvedType::Array(inner, _) = &**inner_array_id {
+                inner_array_id = inner;
+            }
+
+            // We've now reached the innermost type of the array.
+            check_invalid_cyclic_refs_in_field_kind(
+                stores,
+                had_error,
+                root_id,
+                root_name_location,
+                field_name_location,
+                inner_array_id,
+                check_queue,
+                checked_items,
+            );
+        }
+        // All these are indirect, so fine.
+        NameResolvedType::Pointer(_)
+        | NameResolvedType::SimpleBuiltin(_)
+        | NameResolvedType::SimpleGenericParam(_) => {}
+    }
 }
 
 fn check_invalid_cycles_const_assert(
